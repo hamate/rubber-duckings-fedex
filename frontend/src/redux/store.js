299a4527afcca24941a204cf74b905f2a952d203
kisborg <mark.borgstrom@gmail.com>
2021-01-19 09:34:17 +0100

    fixed import issue in store.js

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -4,7 +4,7 @@ import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 
-import rootReducer from './root-reducer';
+import rootReducer from './rootReducer';
 
 const middlewares = [thunk];
 
