91372585066f028ca322f5b45cc520ac1ec5717d
hamate <matehajdu2@gmail.com>
2021-01-20 12:14:57 +0100

    rebase

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,13 +7,10 @@ import {
   Redirect
 } from "react-router-dom";
 import Landing from './pages/Landing';
-<<<<<<< HEAD
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Challenge from './pages/Challenge';
-=======
 import AdminPage from './pages/AdminPage';
->>>>>>> b1a52b8... challenge page design
 import './App.css';
 
 function App() {
@@ -30,21 +27,11 @@ function App() {
     <div className="App">
       <Switch>
         <Route exact path="/" component={Landing} />
-<<<<<<< HEAD
         <Route exact path="/register" component={Register} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/challenge-setting" />
         <Route path="/challenge" component={Challenge} />
-=======
-        <Route exact path="/register" />
-        <Route exact path="/login" />
         <Route exact path="/admin" component={AdminPage}/>
-<<<<<<< HEAD
-        <Route path="/challange" />
->>>>>>> b1a52b8... challenge page design
-=======
-        <Route path="/challenge" />
->>>>>>> 20fa087... fetch working
         <Route exact path="/">
             {tokenExists() ? <Redirect to="/challenge" /> : <Redirect to="/" />}
         </Route>
